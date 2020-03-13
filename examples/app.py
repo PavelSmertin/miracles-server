56c2383306a5222048dd0bdfc83e50fc4464da0c
pashtet <pashtetbezd@gmail.com>
2020-03-13 13:28:28 +0500

    deploy changes

--- examples/app.py
+++ examples/app.py
@@ -45,8 +45,8 @@ DB_HOST = os.environ.get('DB_HOST', 'localhost')
 DB_PORT = os.environ.get('DB_PORT', 5432)
 DB_NAME = os.environ.get('DB_NAME', 'postgres')
 
-#DB_URI = 'postgres+psycopg2://{user}:{password}@{host}:{port}/{db}'.format(
-DB_URI = 'postgres://{user}:{password}@{host}:{port}/{db}'.format(
+DB_URI = 'postgres+psycopg2://{user}:{password}@{host}:{port}/{db}'.format(
+#DB_URI = 'postgres://{user}:{password}@{host}:{port}/{db}'.format(
     user=DB_USER,
     password=DB_PASSWORD,
     host=DB_HOST,
